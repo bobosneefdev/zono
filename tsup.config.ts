32fc726bc58542d8f576e87e4c1155628f44e43c
bobosneefdev <bobosneefbusiness@gmail.com>
2025-12-20 01:28:26 -0800

    fix exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     format: ["esm", "cjs"],
     entry: {
-        "index": "src/index.ts",
         "shared": "src/shared.ts",
         "client": "src/client.ts",
         "server": "src/server.ts",
     },
+    keepNames: true,
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
